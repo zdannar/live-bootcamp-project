9c79795294deaf443a18c1ab04deea9ea2f13633
zdannar <zdannar@github.com>
2025-10-13 20:03:02 -0600

    Just some cleanup

--- auth-service/src/domain/error.rs
+++ auth-service/src/domain/error.rs
@@ -1,10 +1,4 @@
 use crate::domain::{UserStoreError, UserValidationError};
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    Json,
-};
-use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Clone, Error)]
@@ -41,27 +35,3 @@ impl From<UserStoreError> for AuthAPIError {
         }
     }
 }
-
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct ErrorResponse {
-//     pub error: String,
-// }
-
-// impl IntoResponse for AuthAPIError {
-//     fn into_response(self) -> Response {
-//         let (status, error_message) = match self {
-//             AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
-//             AuthAPIError::InvalidCredentials(_s) => {
-//                 // Logging/Tracing could be used here.
-//                 (StatusCode::BAD_REQUEST, "Invalid credentails")
-//             }
-//             AuthAPIError::UnexpectedError => {
-//                 (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error")
-//             }
-//         };
-//         let body = Json(ErrorResponse {
-//             error: error_message.to_string(),
-//         });
-//         (status, body).into_response()
-//     }
-// }
