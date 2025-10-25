eb522084b0ac9063c31d9816048fa9d9eb21c4ad
zdannar <zdannar@github.com>
2025-10-25 10:34:09 -0600

    sp.3.5 : test : verifiy token should return 401 on banned token request.  Test are failing as expected.

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,7 +1,7 @@
 use crate::helpers::{assert_success_and_context_type, get_random_email, TestApp};
 use crate::requests;
 use crate::requests::VerifyTokenRequest;
-use auth_service::domain::Email;
+use auth_service::domain::{BannedTokenStore, Email};
 use auth_service::utils::auth::generate_auth_token;
 
 #[tokio::test]
@@ -28,5 +28,20 @@ async fn should_return_401_if_invalid_token() {
     let response = app
         .post_verify_token(&requests::VerifyTokenRequest { token: "SOMETOKEN" })
         .await;
+    assert_success_and_context_type(&response, 401, None);
+}
+
+#[tokio::test]
+async fn should_return_401_if_banned_token() {
+    let app = TestApp::new().await;
+    let email = Email::parse(get_random_email()).unwrap();
+    let token = generate_auth_token(&email).unwrap();
+    app.banned_token_store.store(&token).await.unwrap();
+
+    let request = VerifyTokenRequest {
+        token: token.clone(),
+    };
+
+    let response = app.post_verify_token(&request).await;
     assert_success_and_context_type(&response, 401, None);
 }
