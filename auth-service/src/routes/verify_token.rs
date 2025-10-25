ce06505125bf76b2f5e8555b944549ccf1e9ee70
zdannar <zdannar@github.com>
2025-10-25 10:38:03 -0600

    sp.3.6 : feature : Update verify auth service to check for banned tokens.

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,13 +1,26 @@
+use crate::domain::BannedTokenStore;
 use crate::utils::auth;
-use crate::AuthAPIError;
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use crate::UserStore;
+use crate::{domain::AuthAPIError, AppState};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
-pub async fn verify_token(
+pub async fn verify_token<T: UserStore, B: BannedTokenStore>(
+    State(state): State<AppState<T, B>>,
     Json(request): Json<TokenVerificationRequest>,
 ) -> Result<impl IntoResponse, AuthAPIError> {
-    // Need to use the request.token.  I need to use the validate thing to make sure it is correct.
-    let Ok(claims) = auth::validate_token(&request.token).await else {
+    let is_banned: bool = state
+        .banned_token_store
+        .exists(&request.token)
+        .await
+        .unwrap()
+        .into();
+
+    if is_banned {
+        return Err(AuthAPIError::InvalidToken);
+    }
+
+    let Ok(_claims) = auth::validate_token(&request.token).await else {
         return Err(AuthAPIError::InvalidToken);
     };
 
